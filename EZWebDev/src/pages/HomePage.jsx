e9e06dee35be6ff4a82e6172280c1d47f5f8fd7e
Samuel Vernick <samuel.vernicks@gmail.com>
2025-04-05 18:13:08 -0700

    Updated Homepage and Navigation

---
+++ EZWebDev/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import '../App.css'
+import 'animate.css';
+
+function HomePage(){
+    return(
+        <div className='home-page'>
+            <h1 class="animate__animated animate__backInDown">EZWebDev</h1>
+            <h2 class="animate__animated animate__backInRight">Learn by doing</h2>
+            <div>Click the "create" button to begin your journey</div>
+        </div>
+    )
+}
+
+export default HomePage

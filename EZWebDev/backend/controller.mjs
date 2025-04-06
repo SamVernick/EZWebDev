4743602ff340efca358723b6f28629a8d567e77c
Bianca Rivera Nales <biancariveranales@gmail.com>
2025-04-05 19:16:17 -0700

    Did some work in controller

--- EZWebDev/backend/controller.mjs
+++ EZWebDev/backend/controller.mjs
@@ -6,13 +6,20 @@ PORT = 3000;
 app.post('/create', (req, res) => {
     let rbody = req.body;
     if(rbody.size <= 60, rbody.margin <= 15, rbody.border <= 10, rbody.padding <= 8) {
-        let newElement = createElement(rbody.size, rbody.margin, rbody.border, rbody.padding);
+        let newElement = model.createElement(rbody.size, rbody.margin, rbody.border, rbody.padding);
         res.status(200).json(newElement);
+    } else {
+        res.status(400).json({"Error": "Invalid request, make sure numbers are in boundaries"});
     }
 });
 
-app.get('/create', (req, res) => {
-
+app.get('/create/:id', (req, res) => {
+    let ID = model.getElement(req.params.id);
+    if(ID) {
+        res.status(200).json(ID);
+    } else {
+        res.status(404).json({"Error": "Element not found"});
+    }
 });
 
 app.put('/create/:id', (req,res) => {

ed05c8c0afcbbd2b1a0134321b3de3b431d4b533
Bianca Rivera Nales <biancariveranales@gmail.com>
2025-04-05 23:57:13 -0700

    Finished backend hopefully, can be tweaked if needed

--- EZWebDev/backend/controller.mjs
+++ EZWebDev/backend/controller.mjs
@@ -3,6 +3,8 @@ import * as model from './model.mjs';
 const app = express();
 PORT = 3000;
 
+app.use(express.json());
+
 app.post('/create', (req, res) => {
     let rbody = req.body;
     if(rbody.size <= 60, rbody.margin <= 15, rbody.border <= 10, rbody.padding <= 8) {
@@ -22,13 +24,27 @@ app.get('/create/:id', (req, res) => {
     }
 });
 
-app.put('/create/:id', (req,res) => {
-    //Needs to be done
-
+app.put('/create/:id', (req, res) => {
+    const ID = model.getElement(req.params.id);
+    let rbody = req.body;
+    if(rbody.size > 60, rbody.margin > 15, rbody.border > 10, rbody.padding > 8) {
+        res.status(400).json({"Error": "Improper values inserted!"});
+    } else if (!ID) {
+        res.status(404).json({"Error": "Element can't be found :("});
+    } else {
+        let updatedElement = model.updateElement(ID, rbody.size, rbody.margin, rbody.border, rbody.padding);
+        res.status(200).json(updatedElement);
+    }
 });
 
-app.delete('/create/:id', (req,res) => {
-
+app.delete('/create/:id', (req, res) => {
+    const element = model.getElement(req.params.id);
+    if(element) {
+        model.deleteElement(element);
+        res.status(204).json(element);
+    } else {
+        res.status(404).json({"Error": "Element can't be found :("});
+    }
 });
 
 app.listen(PORT, () => {
--- EZWebDev/backend/model.mjs
+++ EZWebDev/backend/model.mjs
@@ -1,17 +1,28 @@
-import Element from './element.mjs';
+import elements from './data/elements.mjs';
+import Element from './element.mjs'; //the class for the element being implemented
 
 function createElement(size, margin, border, padding) {
     element = new Element(size, margin, border, padding);
+    elements.push(element);
     return element;
 }
 
-function getElement() {
-
+function getElement(id) {
+    return elements.find(element => element.id == id);
 }
 
-function deleteElement() {
+function updateElement(ID, size, margin, border, padding) {
+    ID.size = size;
+    ID.margin = margin;
+    ID.border = border;
+    ID.padding = padding;
+    return ID;
+}
 
+function deleteElement(element) {
+    elements.splice(elements.indexOf(element), 1);
+    return element;
 }
 
 
-export{ createElement, getElement, deleteElement};
+export{ createElement, getElement, updateElement, deleteElement};
